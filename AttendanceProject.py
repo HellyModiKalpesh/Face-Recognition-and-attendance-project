607853f5773ac26e8e8fe0f35eaecbc5ad981e0b
Helly modi <105526680+HellyModiKalpesh@users.noreply.github.com>
2023-05-05 16:18:03 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -81,16 +81,4 @@ while True:
 
 
 
-# faceLoc=face_recognition.face_locations(imgTest)[0]
-# encodeTest=face_recognition.face_encodings(imgTest)[0]
-# cv2.rectangle(imgTest, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
-#
-# faceLocTest=face_recognition.face_locations(imgElon)[0]
-# encodeElon=face_recognition.face_encodings(imgElon)[0]
-# cv2.rectangle(imgElon, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
-#
-# #less the distance more it will be good
-# results=face_recognition.compare_faces([encodeTest],encodeElon)
-# facedist=face_recognition.face_distance([encodeElon],encodeTest)
-# cv2.putText(imgTest, f'{results} {round(facedist[0],2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
